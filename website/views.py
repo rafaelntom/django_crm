c604b683273011fc68f6338b5018367187f22b0c
Rafael Tomazini <rafaelnt83@gmail.com>
2023-08-30 09:37:56 -0300

    feat: records are now shown for users that are logged in

--- website/views.py
+++ website/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
-import ipdb
+from .forms import SignUpForm
+from .models import Record
 
 
 # Create your views here.
 def home(request):
+    records = Record.objects.all()
+
     if request.method == "POST":
         username = request.POST["username"]
         password = request.POST["password"]
@@ -21,10 +24,28 @@ def home(request):
             )
             return redirect("home")
 
-    return render(request, "home.html")
+    return render(request, "home.html", {"records": records})
 
 
 def logout_user(request):
     logout(request)
     messages.success(request, "You have been logged out")
     return redirect("home")
+
+
+def register_user(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password1"]
+
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You have been successfully registered!")
+            return redirect("home")
+    else:
+        form = SignUpForm()
+
+    return render(request, "register.html", {"form": form})
